5c8c0f6e691ca99a5de5067b78a7ca5c0e4b9b6a
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-03-25 12:47:58 +0300

    chore: selectionType from timeline chart on single view

--- webapp/javascript/pages/ContinuousSingleView.tsx
+++ webapp/javascript/pages/ContinuousSingleView.tsx
@@ -100,6 +100,7 @@ function ContinuousSingleView() {
           id="timeline-chart-single"
           timelineA={getTimeline()}
           onSelect={(from, until) => dispatch(setDateRange({ from, until }))}
+          selectionType="single"
         />
         <Box>{flamegraphRenderer}</Box>
       </div>
